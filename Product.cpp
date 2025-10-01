574204f565bb4384f72121be5dd5cf711ced0f7c
NoSwear4324 <spiridonovaroslav10@gmail.com>
2025-10-01 18:45:40 +0300

    s

--- Product.cpp
+++ Product.cpp
@@ -1,1 +1,46 @@
 #include "Product.h"
+
+Product::Product()
+{
+
+}
+
+Product::Product(string name, float price, float weight)
+{
+
+}
+
+void Product::setName(string name)
+{
+
+}
+
+void Product::setPrice(float price)
+{
+
+}
+
+void Product::setWeight(float weight)
+{
+
+}
+
+string Product::getName() const
+{
+    return string();
+}
+
+float Product::getPrice() const
+{
+    return 0.0f;
+}
+
+float Product::getWeight() const
+{
+    return 0.0f;
+}
+
+void Product::showInfo() const
+{
+
+}
