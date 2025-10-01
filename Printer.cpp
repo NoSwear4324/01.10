7e929ec5f5ae4bde498a9af32325edcdc30c3f19
NoSwear4324 <spiridonovaroslav10@gmail.com>
2025-10-01 17:57:05 +0300

    knrj

--- Printer.cpp
+++ Printer.cpp
@@ -5,5 +5,14 @@ void Printer::printFile(string text, string fileName, int copy)
 	for (int i = 0; i < copy; i++) {
 		string name = "Copy_" + to_string(i) + "_" + fileName;
 		ofstream file(name);
+		if (file.is_open()) {
+			file << text;
+			file.close();
+		}
+	}
+
+	for (int i = 0; i < copy; i++) {
+		string name = "start Copy_" + to_string(i) + "_" + fileName;
+		system(name.c_str());
 	}
 }
